9f54b2d403451da0b863b0c383afe0e36012b14a
nagehancapa <capanagehan@gmail.com>
2022-02-21 13:06:15 +0100

    added mdx file fro documentation

--- src/components/atoms/Button/Button.stories.js
+++ src/components/atoms/Button/Button.stories.js
@@ -2,7 +2,27 @@ import React from "react";
 import { action } from "@storybook/addon-actions";
 import Button from "./Button";
 
-export default { title: "atoms/button" };
+export default {
+  title: "atoms/button",
+  component: Button,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "The **button** is the component that should fire an action based on a click event.",
+      },
+    },
+  },
+  argTypes: {
+    children: {
+      description: "The element(s) that should be rendered within the button",
+      default: "undefined",
+    },
+    onClick: {
+      description: "The action that is fired when the button is pressed",
+    },
+  },
+};
 
 export const Standard = () => (
   <Button onClick={action("Button")}>This is a button</Button>
